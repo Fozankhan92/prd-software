05dffbccb5e022d6b5e09d528ce76825350e7a5b
Fozankhan92 <fozankhan92@gmail.com>
2026-07-15 00:37:44 +0500

    Implement S3CompatibleCloudFileAdapter class

---
+++ apps/web/cloud.ts
@@ -0,0 +1,27 @@
+import type { CloudFileAdapter, CloudFileMetadata, FilePermission } from './storage';
+
+export interface SignedCloudRequest {
+  url: string;
+  storageKey: string;
+}
+
+export interface CloudRequestSigner {
+  signUpload(metadata: CloudFileMetadata, permission: FilePermission): Promise<SignedCloudRequest>;
+  signDownload(file: CloudFileMetadata, permission: FilePermission): Promise<{ url: string }>;
+}
+
+export class S3CompatibleCloudFileAdapter implements CloudFileAdapter {
+  readonly enabled = true;
+
+  constructor(private readonly signer: CloudRequestSigner) {}
+
+  async createUploadRequest(metadata: CloudFileMetadata, permission: FilePermission) {
+    const request = await this.signer.signUpload(metadata, permission);
+    return { uploadUrl: request.url, storageKey: request.storageKey };
+  }
+
+  async createDownloadRequest(file: CloudFileMetadata, permission: FilePermission) {
+    const request = await this.signer.signDownload(file, permission);
+    return { downloadUrl: request.url };
+  }
+}
